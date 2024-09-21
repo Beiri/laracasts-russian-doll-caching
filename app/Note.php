3f6ffa354d33cfd83f3590c746c1cd2ff70110d3
Beiri Alife <beiri.al@outlook.com>
2024-09-21 15:08:38 -0300

    Ep 03 - Russian Doll Caching

--- app/Note.php
+++ app/Note.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
+    use Cacheable;
+
+    protected $touches = ['card'];
+
     public function card()
     {
         return $this->belongsTo(Card::class);
